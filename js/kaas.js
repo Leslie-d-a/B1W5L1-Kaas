368c4a4b9b9de05a8911d27b4a49a16d6ba7e82b
Leslie den Adel <99062688@mydavinci.nl>
2020-09-24 07:27:29 +0200

    klaar

--- js/kaas.js
+++ js/kaas.js
@@ -3,51 +3,71 @@ var answer;
 alert("Voer bij deze antwoorden ja of nee in");
 
 var answer = prompt("Is de kaas geel?");
+answer = answer.toLowerCase();
 if (answer == "ja") {
 	var answer = prompt("Zitten er gaten in?");
+	answer = answer.toLowerCase();
 	if (answer == "ja") {
 		var answer = prompt("Is de kaas belachelijk duur?");
+		answer = answer.toLowerCase();
 		if (answer == "ja") {
-			alert("Emmenthaler");
-		} else if (answer == "nee") {
-			alert("Leerdammer");
-		} else {
+			alert("De kaas is: Emmenthaler");
+		}
+		else if (answer == "nee") {
+			alert("De kaas is: Leerdammer");
+		}
+		else {
 			alert ("U heeft geen ja of nee ingevoerd");
 		}
 
-	} else {
+	}
+	else {
 		var answer = prompt("Is de kaas zo hard als een steen?");
+		answer = answer.toLowerCase();
 		if (answer == "ja") {
-			alert("Pamigiano Reggiano");
-		} else if (answer == "nee") {
-			alert("Goudse kaas");
-		} else {
+			alert("De kaas is: Pamigiano Reggiano");
+		}
+		else if (answer == "nee") {
+			alert("De kaas is: Goudse kaas");
+		}
+		else {
 			alert ("U heeft geen ja of nee ingevoerd");
 		}
 	}
-} else if (answer == "nee") {
+}
+else if (answer == "nee") {
 	var answer = prompt("Heeft de kaas blauwe schimmels?");
+	answer = answer.toLowerCase();
 	if (answer == "ja") {
 		var answer = prompt("Heeft de kaas een korst?");
+		answer = answer.toLowerCase();
 		if (answer == "ja") {
-			alert("Bleu de Rochbaron");
-		} else if (answer == "nee") {
-			alert("Foume d'Ambert");
-		} else {
+			alert("De kaas is: Bleu de Rochbaron");
+		}
+		else if (answer == "nee") {
+			alert("De kaas is: Foume d'Ambert");
+		}
+		else {
 			alert ("U heeft geen ja of nee ingevoerd");
 		}
-	} else if (answer == "nee") {
+	}
+	else if (answer == "nee") {
 		var answer = prompt("Heeft de kaas een korst?");
+		answer = answer.toLowerCase();
 		if (answer == "ja") {
-			alert("Camembert");
-		} else if (answer == "nee") {
-			alert("Mozzarella");
-		} else {
+			alert("De kaas is: Camembert");
+		}
+		else if (answer == "nee") {
+			alert("De kaas is: Mozzarella");
+		}
+		else {
 			alert ("U heeft geen ja of nee ingevoerd");
 		}
-	} else {
+	}
+	else {
 		alert ("U heeft geen ja of nee ingevoerd");
 	}
-} else {
+}
+else {
 	alert ("U heeft geen ja of nee ingevoerd");
 }
